cdcb8cfdec9a9c82e78dc71027aa84dc28b3f92b
aaqmarinaa <33062308+aaqmarinaa@users.noreply.github.com>
2017-12-08 21:50:33 +0700

    Update touch.c

--- touch.c
+++ touch.c
@@ -7,14 +7,15 @@
 
 int main(int argc, char *argv[])
 {
+	int fd;
 	if(argc < 2){
 	printf(2, "Touch files write 2 arguments.\n");
 	}
 
-for(int i=1;i<=argc;i++)
+for(int i=0;i<=argc;i++)
 {
 	if((fp = open(argv[i], O_CREATE | O_RDWR)) < 0){
-		printf(2, "touch: %s failed to create\n", argv[i]);
+		printf(1, "touch: %s failed to create\n", argv[i]);
 	}
 	close(fp);
 }	
