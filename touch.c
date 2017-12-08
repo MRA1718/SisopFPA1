96eaca3c85fd1b100125f09827341c9b81a78c08
aaqmarinaa <33062308+aaqmarinaa@users.noreply.github.com>
2017-12-08 21:44:37 +0700

    lupa closenya

--- touch.c
+++ touch.c
@@ -1,19 +1,22 @@
+
 #include "types.h"
 #include "stat.h"
 #include "user.h"
 #include "fcntl.h"
+#include "syscall.h"
 
 int main(int argc, char *argv[])
 {
-  int i;
-  if(argc < 2){
-    printf(2, "Touch files with 2 arguments.\n");
-    exit();
-  }
-    for(i = 1; i < argc; i++){
-    if(fp = open(argv[i], O_CREATE | O_RDWR) < 0){
-       printf(2, "touch: %s failed to create\n", argv[i]);
-       break;
-    }
-  }    
+	if(argc < 2){
+	printf(2, "Touch files write 2 arguments.\n");
+	}
+
+for(int i=1;i<=argc;i++)
+{
+	if((fp = open(argv[i], O_CREATE | O_RDWR)) < 0){
+		printf(2, "touch: %s failed to create\n", argv[i]);
+	}
+	close(fp);
+}	
+	exit();
 }
