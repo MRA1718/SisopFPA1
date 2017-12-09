90cf8d62001cdb947364ec7607d0edcff58b9180
fantazqis <33064566+fantazqis@users.noreply.github.com>
2017-12-09 15:24:02 +0700

    Update rm.c

--- rm.c
+++ rm.c
@@ -7,18 +7,18 @@ main(int argc, char *argv[])
 {
   int i;
 
-  if(argc < 2) //mengecek parameter, jika terpenuhi
+  if(argc < 2) //mengecek parameter, jika inputan salah
   {
-    printf(2, "Usage: rm files...\n");
+    printf(2, "rm [filename]a\n");
     exit();
   }
 
   for(i = 1; i < argc; i++) //looping untuk unlink
   {
-    if(unlink(argv[i]) < 0) //menghapus file dengan unlink(menghaous pointer)
+    if(unlink(argv[i]) < 0) //menghapus file dengan unlink
 	{
       printf(2, "rm: %s failed to delete\n", argv[i]); // jika nilai argv negatif maka akan muncul pesan failed
-      break;
+      exit();
     }
   }
 
