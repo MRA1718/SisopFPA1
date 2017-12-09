ed34d396695070557cfe1c01274fcc5522a2abfa
MRA1718 <33035393+MRA1718@users.noreply.github.com>
2017-12-09 12:50:47 +0700

    update

--- cd.c
+++ cd.c
@@ -5,5 +5,10 @@
 
 int main(int argc, char *argv[])
 {
-
+  //mengecek banyaknya paramater yang akan dieksekusi
+  if(argc < 2) //terpenuhi jika command cd dipanggi tidak terdapat destinasi path yang dituju
+  {
+    printf(1,"cd need arguments [path destination]");
+    exit();
+  }
 }
