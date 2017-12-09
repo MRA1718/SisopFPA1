7cf58872c8207fedcc70bf3a4a97add7e38efc89
aaqmarinaa <33062308+aaqmarinaa@users.noreply.github.com>
2017-12-09 14:48:29 +0700

    fixxx

--- touch.c
+++ touch.c
@@ -9,15 +9,14 @@ int main(int argc, char *argv[])
 {
 	int fp;
 	if(argc < 2){
-	printf(2, "Touch files with 2 arguments.\n");
+	printf(1, "Touch files with 2 arguments.\n");
 	}
-
-for(int i=0;i<=argc;i++)
-{
-	if((fp = open(argv[i], O_CREATE | O_RDWR)) < 0){
-		printf(2, "touch: %s failed to create\n", argv[i]);
-	}
-	close(fp);
-}	
+	for(int i=0;i<=argc;i++)
+	{
+		if((fp = open(argv[i], O_CREATE | O_RDWR)) < 0){
+			printf(2, "touch: %s failed to create\n", argv[i]);
+		}
+		close(fp);
+	}	
 	exit();
 }
