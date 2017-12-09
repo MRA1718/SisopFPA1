3bf2b94d6abf2d6838cb0e2f60bf4f3dd502b073
MRA1718 <33035393+MRA1718@users.noreply.github.com>
2017-12-09 12:57:15 +0700

    Update cd.c

--- cd.c
+++ cd.c
@@ -8,7 +8,15 @@ int main(int argc, char *argv[])
   //mengecek banyaknya paramater yang akan dieksekusi
   if(argc < 2) //terpenuhi jika command cd dipanggi tidak terdapat destinasi path yang dituju
   {
-    printf(1,"cd need arguments [path destination]");
+    printf(1, "cd need arguments [path destination]\n");
     exit();
   }
+  
+  //mengecek isi dari argv[1] apakah ada atau tidaknya path yg dituju
+  if(chdir(argv[1]) < 0) //jika mengeluarkan nilai negatif, maka muncul pesan error
+  {
+    printf(1, "Failed to change directory to *s!\n", argv[1]);
+  }
+  
+  exit(); // jika kondisi diatas tidak ada yg terpenuhi maka cd di eksekusi dan program exit
 }
